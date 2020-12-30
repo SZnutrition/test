3b4429f8e3df529aaceec73b3e3d7603fa34a3fe
SZnutrition <76773416+SZnutrition@users.noreply.github.com>
2020-12-30 16:41:42 +0500

    Release (very soon)

---
+++ analyzes/list.js
@@ -0,0 +1,65 @@
+let analyzes = [
+    {
+        analyzeName: 'Анемия',
+        analyzeHref: 'how.html',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'Общий анализ крови\n Ферритин\n В9\n В12\n Трансферрин\n ОЖСС\n',
+        analyzeImg: "assets/pics/anemia_diagnostic.jpg",
+        analyzeBtn: "КАК РАСШИФРОВАТЬ АНАЛИЗЫ"
+    },
+    {
+        analyzeName: 'Гипотиреоз',
+        analyzeHref: 'how.html',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'ТТГ\n Т3 св.\n Т4 св.\n АТ-ТПО\n',
+        analyzeImg: "assets/pics/gipo_diagnostic.jpg",
+        analyzeBtn: "КАК РАСШИФРОВАТЬ АНАЛИЗЫ"
+    },
+    {
+        analyzeName: 'Инсулин',
+        analyzeHref: 'how.html',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'Глюкоза\n Инсулин\n Индекс НОМА\n Индекс Caro\n',
+        analyzeImg: "assets/pics/insulin_diagnostic.jpg",
+        analyzeBtn: "КАК РАСШИФРОВАТЬ АНАЛИЗЫ"
+    },
+    {
+        analyzeName: 'Кандидоз',
+        analyzeHref: 'what.html',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'Не существует 100% диагностики, симптомы сами по себе специфичны.\n',
+        analyzeImg: "assets/pics/candida_diagnostic.jpg",
+        analyzeBtn: "ЧТО С ЭТИМ ДЕЛАТЬ"
+    },
+    {
+        analyzeName: 'Белок',
+        analyzeHref: 'how.html',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'Общий белок\n Альбумин\n Копрограмма\n',
+        analyzeImg: "assets/pics/belok_diagnostic.jpg",
+        analyzeBtn: "КАК РАСШИФРОВАТЬ АНАЛИЗЫ"
+    },
+    {
+        analyzeName: 'Магний',
+        analyzeHref: 'https://www.instagram.com/p/CAr_3SVpxog/',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'Не существует показательного анализа на дефицит магния. Если вы испытываете данные симптомы, то необходимо принимать магний в добавках.',
+        analyzeImg: "assets/pics/magnesium_diagnostic.jpg",
+        analyzeBtn: "Пост про магний"
+    },
+    {
+        analyzeName: 'СИБР',
+        analyzeHref: 'what.html',
+        analyzeSrc:'anemia.js',
+        analyzeScore: 'anemia_score.js',
+        analyzeDescription: 'Не существует 100% диагностики, симптомы сами по себе специфичны.\n',
+        analyzeImg: "assets/pics/sibo_diagnostic.png",
+        analyzeBtn: "ЧТО С ЭТИМ ДЕЛАТЬ"
+    }
+];
